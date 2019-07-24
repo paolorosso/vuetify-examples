c8c14156b68e484e2c63471bb23ac4be4765a18f
Paolo Rosso <paolo@i2k.it>
2019-07-24 16:09:27 +0200

    init

---
+++ Dialog-model-computed.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-dialog v-model="getDialog" max-width="1200px"></v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: { type: Boolean, required: true, default: false }
+  },
+
+  methods: {
+    close() {
+      this.$emit("close");
+    }
+  },
+
+  computed: {
+    getDialog: {
+      get: function() {
+        return this.dialog;
+      },
+      set: function(value) {
+        if (!value) {
+          this.close();
+        }
+      }
+    }
+  }
+};
+</script>
+
+
